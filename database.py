99e2c2a581c374c063a71beab94cf72f2ff24427
Ailen-Aspe <ailengraceaspe@gmail.com>
2017-05-04 10:50:01 -0700

    Database.pu has been set-up

--- database.py
+++ database.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
 import os
 
-#some of the codes are from the tasks sample and re-implemented
+
+# some of the codes are from the tasks sample and re-implemented
 
 class DBconnection:
     def __init__(self):
-            engine =create_engine("postgres://postgres:postgres@127.0.0.1:5432/NAInfoSys", echo=False)
+        engine = create_engine("postgres://postgres:postgres@127.0.0.1:5432/NAInfoSys", echo=False)
+        self.conn = engine.connect()
+        self.trans = self.conn.begin()
 
+    def getcursor(self):
+        cursor = self.conn.connection.cursor()
+        return cursor
+
+    def dbcommit(self):
+        self.trans.commit()
