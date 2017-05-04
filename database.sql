1e885cc726bf8bd0e136b5d7f4764751c723a0bb
Ailen-Aspe <ailengraceaspe@gmail.com>
2017-05-04 11:01:55 -0700

    Database is modified

--- database.sql
+++ database.sql
@@ -1,2 +1,20 @@
 --create database NAIS
 
+create table focal(
+    id int8 primary key,
+    first_name text,
+    last_name text,
+    designation text
+);
+create table supervisors(
+
+    id int8 primary key,
+    first_name text,
+    last_name text
+);
+
+create table children(
+
+
+);
+create or replace function
