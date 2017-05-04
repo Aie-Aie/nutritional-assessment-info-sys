75c94dae924a42c93d6634017ffac5e205215407
Ailen-Aspe <ailengraceaspe@gmail.com>
2017-05-04 10:19:31 -0700

    Database.sql is added for Documentation purposes in Postgres

--- database.py
+++ database.py
@@ -1,4 +1,9 @@
 from sqlalchemy import create_engine
 import os
 
+#some of the codes are from the tasks sample and re-implemented
+
 class DBconnection:
+    def __init__(self):
+            engine =create_engine("postgres://postgres:postgres@127.0.0.1:5432/NAIS", echo=False)
+
