a9633bda9af6755669ff4cb32f101d0ab00f1a1b
Ailen-Aspe <ailengraceaspe@gmail.com>
2017-05-05 16:55:12 -0700

    SP updatefocal added

--- database.sql
+++ database.sql
@@ -81,6 +81,7 @@ create or replace function dropfocal(par_id int8) returns text as
                 loc_res ='Data deleted';
             end if;
                 return loc_res;
+		end;
     $$
         language 'plpgsql';
 
@@ -90,17 +91,21 @@ create or replace function updatefocal(par_id int8, par_designation text) return
 			loc_id text;
 			loc_res text;
 		begin
-			select into focal where id= par_id;
+			select into loc_id id from focal where id= par_id;
 			if loc_id isnull then
 				loc_res = 'Data not found';
 			else
 				--update query
+				update focal set designation = par_designation where id=par_id;
 				loc_res ='Data updated.';
 			end if;
 				return loc_res;
+		end;
 	$$
-		language 'plpgsql';
 
+		language 'plpgsql';
+	
+	--select updatefocal(1, 'Canaway');
 
 
 
