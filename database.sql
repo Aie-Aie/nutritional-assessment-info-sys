145c314505d1391963061ce8a0038a7deb2f4c0b
Ailen-Aspe <ailengraceaspe@gmail.com>
2017-05-05 00:43:44 -0700

    drop SP is on the process

--- database.sql
+++ database.sql
@@ -14,15 +14,6 @@ create table supervisors(
 
 );
 
-create table children(
-   id int8 primary key,
-    first_name text,
-    last_name text,
-    weight real,
-    height real,
-    BMI real
-
-);
 create or replace function newfocal(par_id int8, par_fname text, par_lname text, par_designation text) returns text as
 $$
     declare
@@ -46,5 +37,45 @@ $$
 --insert into supervisors(id, first_name, last_name) values (1364, 'Mycroft', 'Holmes');
 --insert into supervisors(id, first_name, last_name) values (2013, 'Allan', 'Turing');
 
-create or replace function
+
+create table children(
+   id int8 primary key,
+    first_name text,
+    last_name text,
+    weight real,
+    height real,
+    BMI text
+
+);
+
+create or replace function newChild(par_id int8, par_fname text, par_lname text, par_weight real, par_height real, par_BMI text) returns text as
+$$
+    declare
+        loc_id text;
+        loc_res text;
+    begin
+        select into loc_id id from children where id= par_id;
+        if loc_id isnull then
+            insert into children(id, first_name, last_name, weight, height, BMI) values (par_id, par_fname, par_lname, par_weight, par_height, par_BMI);
+            loc_res ='New child added';
+        else
+            loc_res ='Data exists'
+        end if;
+            return loc_res;
+        end;
+$$
+    language 'plpgsql';
+
+create or replace function dropfocal(par_id int8) returns text as
+    $$
+        declare
+            loc_id text;
+            loc_res text;
+        begin
+            select into loc_id from focal where id =par_id;
+            if loc_id isnull then
+                loc_res ='Data Not Found.';
+            else
+
+
 
